61103630f27163af7c662e96adf976852574e7db
Fran Araneda <101838257+Cefir-Araneda@users.noreply.github.com>
2023-05-10 11:31:08 -0400

    validador pass arreglar

--- js/validador.js
+++ js/validador.js
@@ -1,9 +1,18 @@
+var clave = document.getElementById('clave');
 // Validar mail
 function validarEmail(email) {
+    console.log('Enviando formulario...');
     expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if ( !expr.test(email) )
         alert("Error: La dirección de correo " + email + " es incorrecta.");
         return false;
+}
+function validarPass(clave) {
+    if(clave.value === null || clave.value === '' || clave.value.length < 8) {
+        mensajesError.push('Ingresa una contraseña valida')
+    } else {
+        clave.value = SHA1(clave.value);	
+    }
 }
 function cifrar(){
         var input_pass = document.getElementById("clave");
