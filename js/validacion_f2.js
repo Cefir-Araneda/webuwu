39b6c7db74f35d284f262ef40c4f14be9fc4b8dc
Fran Araneda <101838257+Cefir-Araneda@users.noreply.github.com>
2023-05-10 11:17:16 -0400

    validaciones

---
+++ js/validacion_f2.js
@@ -0,0 +1,33 @@
+var nombre = document.getElementById('nombre');
+var modelo = document.getElementById('modelo');
+var patente = document.getElementById('patente');
+var diagnostico = document.getElementById('diagnostico');
+var valor = document.getElementById('valor');
+var error = document.getElementById('error');
+
+function enviarFormulario() {
+    console.log('Enviando formulario...');
+    var mensajesError = [];
+    let valorInt = parseInt(valor.value);
+
+    if(nombre.value === null || nombre.value === '') {
+        mensajesError.push('Ingresa nombre')
+    }
+    
+    if(modelo.value === null || nombre.value === '') {
+        mensajesError.push('Ingresa un modelo')
+    }
+    if(patente.value === null || patente.value === '' || patente.value.length !=6) {
+        mensajesError.push('Ingresa patente correcta')
+    }
+    if(diagnostico.value === null || diagnostico.value === '' || diagnostico.value.length < 20) {
+        mensajesError.push('Ingresa un diagnostico valido')
+    }
+    if(valor.value === null || valor.value === '' ||!Number.isInteger(valorInt)) {
+        mensajesError.push('Ingresa el valor total')
+    }
+
+    error.innerHTML = mensajesError.join(', ');
+
+    return false;
+}
