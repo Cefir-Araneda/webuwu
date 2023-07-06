07c7819d1e4e03540ec9436a5c17453a011843f4
Aidan Asher <129128793+ColdWolff@users.noreply.github.com>
2023-07-06 19:13:43 -0300

    Update forms.py

--- taller/forms.py
+++ taller/forms.py
@@ -5,4 +5,7 @@ from django.forms import ModelForm
 class AtencionForm(ModelForm):
     class Meta:
         model = Atencion
-        fields = '__all__'
+        fields = ["mecanico","fecha_atencion","modelo","patente","diagnostico","id_mantencion","cantidad","valor"]
+        labels =  {'mecanico':'Mecanico','fecha_atencion':'Fecha (año-mes-dia)','modelo':'Modelo',
+                  'patente':'Patente','diagnostico':'Diagnostico','id_mantencion':'Tipo de mantencion',
+                  'cantidad':'Cantidad','valor':'Valor total'}
